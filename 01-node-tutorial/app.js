e976eaaf17d10fd67235a9f26f0ca480bcd9b45a
aadityaneve <aditya.neve@gmail.com>
2021-07-07 20:54:10 +0530

    first commit

--- 01-node-tutorial/app.js
+++ 01-node-tutorial/app.js
@@ -1,1 +1,16 @@
 console.log('Welcome to Node Tutorial')
+
+
+const { readFile, writeFile } = require('fs')
+
+console.log('started a first task')
+// CHECK FILE PATH!!!!
+readFile('./content/first.txt', 'utf8', (err, result) => {
+  if (err) {
+    console.log(err)
+    return
+  }
+  console.log(result)
+  console.log('completed first task')
+})
+console.log('starting next task')
